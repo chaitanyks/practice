49f249de07ea08bd7f6ff5ccafe9817d6c593321
chaitanya <defcdr@gmail.com>
2020-06-01 12:53:47 +0530

    main chages

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void fab(int n){
-    int a=0;
-    int b=1;
+void fibonacci(int n){
+    int t1=0;
+    int t2=1;
     for(int i=0;i<=n;i++){
-        cout<<a+b<<endl;
+        cout<<t1+t2<<endl;
         int temp=a;
-        a=b;
-        b=b+temp;
+        t1=t2;
+        t2=t1+temp;
     }
 }
 
 int main(){
     int n;
     n=5;
-    fab(n);
+    fibonacci(n);
 }
