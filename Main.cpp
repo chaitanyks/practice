94a5693dd63830f6d55aca97a8346de776dd84ee
chaitanya <defcdr@gmail.com>
2020-06-01 14:34:36 +0530

    ol

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 void fibonacci(int n){
-    int t1=10;
-    int t2=10;
-    int t3=5;
+    int t1=1;
+    int t2=0;
     for(int i=0;i<=n;i++){
         cout<<t1+t2<<endl;
         int temp=t1;
