1ec6d967525709d7d094859d6835e87ecfd1de36
chaitanya <defcdr@gmail.com>
2020-06-02 13:36:19 +0530

    main changes

--- Main.cpp
+++ Main.cpp
@@ -2,19 +2,17 @@
 using namespace std;
 
 void fibonacci(int n){
-    int t1=10;
-    int t2=10;
-    int t3=5;
+    int x=1,y=0,temp;
     for(int i=0;i<=n;i++){
-        cout<<t1+t2<<endl;
-        int temp=t1;
-        t1=t2;
-        t2=t1+temp;
+        cout<<x+y<<endl;
+        temp=x;
+        x=y;
+        y+=temp;
 }
 }
 
 int main(){
     int n;
-    n=5;
+    n=100;
     fibonacci(n);
 }
